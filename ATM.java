b92a100a51eb6305d105126cca900d697c1a2740
Vostrikov-1-9 <90481123+Vostrikov-1-9@users.noreply.github.com>
2026-06-29 21:40:50 +0300

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -2,81 +2,87 @@ import java.util.Scanner;
 
 public class ATM {
     private BankAccount account;
-    private Scanner scanner;
 
     public ATM(BankAccount account) {
         this.account = account;
-        this.scanner = new Scanner(System.in);
     }
 
-    public void start() {
-        boolean exit = false;
-        while (!exit) {
-            showMenu();
-            System.out.print("> ");
-            
-            if (!scanner.hasNextInt()) {
-                System.out.println("Invalid input. Please enter a number.");
-                scanner.next();
-                continue;
-            }
-            
-            int choice = scanner.nextInt();
-
-            switch (choice) {
-                case 1:
-                    System.out.println("Your balance: " + account.getBalance());
-                    break;
-                case 2:
-                    System.out.print("How much money to deposit?\n> ");
-                    long depositAmount = scanner.nextLong();
-                    if (depositAmount > 0) {
-                        account.deposit(depositAmount);
-                        System.out.println("Success!");
-                    } else {
-                        System.out.println("Amount must be greater than 0");
-                    }
-                    break;
-                case 3:
-                    System.out.print("How much money to withdraw?\n> ");
-                    long withdrawAmount = scanner.nextLong();
-                    
-                    if (withdrawAmount > 0) {
-                        if (account.withdraw(withdrawAmount)) {
-                            dispenseMoney(withdrawAmount);
-                        } else {
-                            System.out.println("Not enough money");
-                        }
-                    } else {
-                        System.out.println("Amount must be greater than 0");
-                    }
-                    break;
-                case 4:
-                    exit = true;
-                    System.out.println("Goodbye!");
-                    break;
-                default:
-                    System.out.println("Invalid choice, try again.");
-            }
-            System.out.println();
-        }
-    }
-
-    private void showMenu() {
+    public void showMenu() {
         System.out.println("1. Check balance");
         System.out.println("2. Deposit money");
         System.out.println("3. Withdraw money");
         System.out.println("4. Exit");
     }
 
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+        
+        while (true) {
+            showMenu();
+            System.out.print("> ");
+            
+            // Защита от ввода букв вместо цифр
+            if (!scanner.hasNextInt()) {
+                System.out.println("Error: enter a number!");
+                scanner.next(); // чистим буфер
+                continue;
+            }
+            
+            int action = scanner.nextInt();
+
+            if (action == 1) {
+                System.out.println("Your balance: " + account.getBalance());
+                
+            } else if (action == 2) {
+                System.out.println("How much money to deposit?");
+                System.out.print("> ");
+                long money = scanner.nextLong();
+                
+                if (money > 0) {
+                    account.deposit(money);
+                    System.out.println("Success!");
+                } else {
+                    System.out.println("Amount must be greater than 0");
+                }
+                
+            } else if (action == 3) {
+                System.out.println("How much money to withdraw?");
+                System.out.print("> ");
+                long money = scanner.nextLong();
+                
+                if (money > 0) {
+                    boolean success = account.withdraw(money);
+                    if (success) {
+                        dispenseMoney(money);
+                    } else {
+                        System.out.println("Not enough money");
+                    }
+                } else {
+                    System.out.println("Amount must be greater than 0");
+                }
+                
+            } else if (action == 4) {
+                System.out.println("Goodbye!");
+                break; // выходим из бесконечного цикла
+                
+            } else {
+                System.out.println("Wrong command, try again");
+            }
+            
+            System.out.println(); // пустая строка в конце цикла
+        }
+    }
+
     private void dispenseMoney(long amount) {
-        long remaining = amount;
-        for (Denomination denomination : Denomination.values()) {
-            int denomValue = denomination.getValue();
-            if (remaining >= denomValue) {
-                long count = remaining / denomValue;
-                System.out.println(denomValue + " x " + count);
-                remaining %= denomValue;
+        long leftToPay = amount; // сколько осталось выдать
+        
+        for (Denomination d : Denomination.values()) {
+            int nominal = d.getValue();
+            
+            if (leftToPay >= nominal) {
+                long count = leftToPay / nominal;
+                System.out.println(nominal + " x " + count);
+                leftToPay = leftToPay % nominal;
             }
         }
     }
