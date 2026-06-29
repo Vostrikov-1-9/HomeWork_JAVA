dd0d615a15245cfc30e4f30e522b8778bc4c4496
Vostrikov-1-9 <90481123+Vostrikov-1-9@users.noreply.github.com>
2026-06-29 21:41:11 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        BankAccount account = new BankAccount("Иван Иванов", 1000);
-        ATM atm = new ATM(account);
+        BankAccount myAccount = new BankAccount("Test User", 5000);
+        ATM atm = new ATM(myAccount);
+        
         atm.start();
     }
 }
